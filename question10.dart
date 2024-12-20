6bd9fbb7adde4c5e8e16f1f72ae7e269d13d9a30
tragicarachnid <167139841+tragicarachnid@users.noreply.github.com>
2024-12-21 00:22:36 +0530

    Update question10.dart

--- question10.dart
+++ question10.dart
@@ -1,4 +1,26 @@
-void main()
-{
+void main() {
+  List<int> numbers = [1, 3, 5, 7, 9, 11];
+  int target = 7;
+  int index = binarySearch(numbers, target);
   
+  if (index != -1) {
+    print("The number $target is at index $index.");
+  } else {
+    print("The number $target is not in the list.");
+  }
+}
+
+int binarySearch(List<int> list, int target) {
+  int left = 0, right = list.length - 1;
+  while (left <= right) {
+    int mid = left + (right - left) ~/ 2;
+    if (list[mid] == target) {
+      return mid;
+    } else if (list[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
